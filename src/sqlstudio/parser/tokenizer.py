71ce8664b71befdf6d20181601d2d66a77a83f1c
Rubén <seijoruben@gmail.com>
2026-07-22 17:28:52 +0200

    Add

---
+++ src/sqlstudio/parser/tokenizer.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import List
+
+from .ast import Token
+
+
+class SQLTokenizer:
+    def __init__(self, sql_text: str):
+        self.sql_text = sql_text
+        self.position = 0
+
+    def tokenize(self) -> List[Token]:
+        tokens: List[Token] = []
+        while self.position < len(self.sql_text):
+            char = self.sql_text[self.position]
+            if char.isspace():
+                self.position += 1
+                continue
+            if char in "(),;":
+                tokens.append(Token(kind="symbol", value=char, position=self.position))
+                self.position += 1
+                continue
+            if char in "'\"":
+                start = self.position
+                self.position += 1
+                while self.position < len(self.sql_text) and self.sql_text[self.position] != char:
+                    self.position += 1
+                if self.position < len(self.sql_text):
+                    self.position += 1
+                tokens.append(Token(kind="literal", value=self.sql_text[start:self.position], position=start))
+                continue
+            if char.isalnum() or char in "._#@":
+                start = self.position
+                self.position += 1
+                while self.position < len(self.sql_text) and (self.sql_text[self.position].isalnum() or self.sql_text[self.position] in "._#@"):
+                    self.position += 1
+                tokens.append(Token(kind="identifier", value=self.sql_text[start:self.position], position=start))
+                continue
+            tokens.append(Token(kind="operator", value=char, position=self.position))
+            self.position += 1
+        return tokens
