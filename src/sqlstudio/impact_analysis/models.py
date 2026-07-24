9fb1c7cb77c7fed0dae83caf7eedbc5878b26ffe
Rubén <seijoruben@gmail.com>
2026-07-24 13:12:05 +0200

    add

--- src/sqlstudio/impact_analysis/models.py
+++ src/sqlstudio/impact_analysis/models.py
@@ -2,6 +2,12 @@ from dataclasses import dataclass, field
 from typing import List
 
 @dataclass
+class ImpactNode:
+    name:str
+    children:List["ImpactNode"]=field(default_factory=list)
+
+@dataclass
 class ImpactResult:
-    root_object: str
-    impacted_objects: List[str] = field(default_factory=list)
+    root_object:str
+    impacted_objects:List[str]=field(default_factory=list)
+    tree:ImpactNode|None=None
